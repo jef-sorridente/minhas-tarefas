1cff6b56fcbc8e8425cd79b60047ea7405427cbb
Jeferson Soares <jpadilha53@gmail.com>
2024-08-06 11:23:55 -0300

    criando o reducer e o slice de busca

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -6,14 +6,22 @@ import { RootReducer } from '../../store'
 
 const ListaDeTarefas = () => {
   const { itens } = useSelector((state: RootReducer) => state.tarefas)
+  const { termo } = useSelector((state: RootReducer) => state.filtro)
+
+  const filtraTarefas = () => {
+    return itens.filter(
+      (item) =>
+        item.titulo.toLocaleLowerCase().search(termo.toLocaleLowerCase()) >= 0
+    )
+  }
 
   return (
     <Container>
       <p>
-        2 tarefas marcadas como: &quot;categoria&ldquo; e &quot;termo&ldquo;
+        2 tarefas marcadas como: &quot;categoria&ldquo; e &quot;{termo}&ldquo;
       </p>
       <ul>
-        {itens.map((t) => (
+        {filtraTarefas().map((t) => (
           <li key={t.id}>
             <Tarefa
               id={t.id}
